e1196fae8a981100702f841ddd7cc1a566191239
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2020-02-25 19:41:40 +0300

    Fix build on Linux (cyrus-sasl-2.1.26.patch, /dev/null for not exists files)

--- conanfile.py
+++ conanfile.py
@@ -12,7 +12,7 @@ def get_safe(options, name):
 
 class CyrusSaslConan(ConanFile):
     name = "cyrus-sasl"
-    version = "2.1.26+0"
+    version = "2.1.26+1"
     license = "Apache License v2.0"
     description = "Cyrus SASL C++ library"
     url = "https://github.com/cyrusimap/cyrus-sasl"
